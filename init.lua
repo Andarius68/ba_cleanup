e964438e810fe220477ebda784571b4aaf17264c
Troy Young / Cianaodh Óg <48970173+Andarius68@users.noreply.github.com>
2024-04-16 13:15:31 -0500

    Add toolrank support to rainbow and nether tools

--- init.lua
+++ init.lua
@@ -8,3 +8,15 @@ minetest.register_abm({
         minetest.set_node(pos, {name="nether:rack"})
     end,
 })
+
+-- Add toolrank support to tools that are missing it.
+
+toolranks.add_tool("nether:pick_nether")
+toolranks.add_tool("nether:shovel_nether")
+toolranks.add_tool("nether:axe_nether")
+toolranks.add_tool("nether:sword_nether")
+toolranks.add_tool("nether:lightstaff")
+toolranks.add_tool("nether:lightstaff_eternal")
+toolranks.add_tool("rainbow_ore:rainbow_ore_pickaxe")
+toolranks.add_tool("rainbow_ore:rainbow_ore_axe")
+toolranks.add_tool("rainbow_ore:rainbow_ore_shovel")
